d1c29b85af34d527fc5b2642a6bea216fd8a0fb4
FuFuWarrior <akpuwarrior@gmail.com>
2022-08-16 21:12:09 +0100

    deploy v3

--- knexfile.js
+++ knexfile.js
@@ -28,7 +28,6 @@ module.exports = {
       port : process.env.DB_PORT,
       user : process.env.DB_USER,
       password : process.env.DB_PASSWORD,
-      database : process.env.DB_NAME,
       migrations: {
         directory: __dirname + '/migrations'
       },
