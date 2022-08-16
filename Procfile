c0902b6e80b3164adebc7263dc7da8811428ca30
FuFuWarrior <akpuwarrior@gmail.com>
2022-08-16 20:44:17 +0100

    deploy v1

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: npm run rollback:all && npm run migrate:seed:account_types && npm start
--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,5 +1,4 @@
 const jwt  = require('jsonwebtoken');
-require('dotenv').config()
 
 
 exports.verifyUser = (req, res, next) => {
