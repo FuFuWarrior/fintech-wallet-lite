cfc09ff2d0f517735a299b51556100f6a666ffa0
FuFuWarrior <akpuwarrior@gmail.com>
2022-08-17 07:33:20 +0100

    made changes

--- src/controllers/fundAccountCtrl.js
+++ src/controllers/fundAccountCtrl.js
@@ -23,104 +23,113 @@ exports.validate = (method) => {
 exports.fundAccountWithCard = async (req, res) => {
     const {user_id, account_id, email} = req;
     const {number, cvv, expiry_year, expiry_month, amount} = req.body;
-   
-    const PAYSTACK_CHARGE_URL = 'https://api.paystack.co/charge'
-    
-    try {
-        const koboAmount = convertNairaToKobo(amount);
 
-        const charge = await axios.post(PAYSTACK_CHARGE_URL, {
-            card: {
-              number,
-              cvv,
-              expiry_year,
-              expiry_month,
-            },
-            email,
-            amount: koboAmount,
-          }, {
-            headers: {
-              Authorization: `Bearer ${process.env.PAYSTACK_SECRET_KEY}`,
-            },
-          });
-          
-        const nextAction = processInitialCardCharge(charge.data);
-        
-        const card = await knex('card_transactions').insert(
-          {
-            external_reference: nextAction.data.reference,
-            amount,
-            account_id,
-            last_response: nextAction.success ? nextAction.message : nextAction.error
-          }
-        );
+    const PAYSTACK_CHARGE_URL = 'https://api.paystack.co/charge';
 
-        if (!nextAction.success) {
-          return res.status(404).json({
-            status: 'error',
-            error: nextAction.error,
-          }); 
-        }
+     const account = await knex.select().from('accounts').where('id', account_id);
 
-        const trx = await knex.transaction();      
+    if (account.length > 0){
         try {
+          const koboAmount = convertNairaToKobo(amount);
 
-          if (nextAction.data.shouldCreditAccount) {
-            const creditResult = await creditAccount({
+          const charge = await axios.post(PAYSTACK_CHARGE_URL, {
+              card: {
+                number,
+                cvv,
+                expiry_year,
+                expiry_month,
+              },
+              email,
+              amount: koboAmount,
+            }, {
+              headers: {
+                Authorization: `Bearer ${process.env.PAYSTACK_SECRET_KEY}`,
+              },
+            });
+            
+          const nextAction = processInitialCardCharge(charge.data);
+          
+          const card = await knex('card_transactions').insert(
+            {
+              external_reference: nextAction.data.reference,
               amount,
               account_id,
-              purpose: 'deposit',
-              metadata: {
-                external_reference: nextAction.data.reference,
-              },
-              trx,
-            });
-
-            if (!creditResult.success) {
-              await trx.rollback();
-
-              return {
-                success: false,
-                error: creditResult.error,
-              };
+              last_response: nextAction.success ? nextAction.message : nextAction.error
             }
+          );
 
-            await trx.commit();
-
-            res.status(200).json({
-              status: 'success',
-              data:{
-                message: 'your account has been funded'
-              }
-            })
+          if (!nextAction.success) {
+            return res.status(404).json({
+              status: 'error',
+              error: nextAction.error,
+            }); 
           }
 
-        } catch (error) {
-          
-          await trx.rollback();
+          const trx = await knex.transaction();      
+          try {
 
+            if (nextAction.data.shouldCreditAccount) {
+              const creditResult = await creditAccount({
+                amount,
+                account_id,
+                purpose: 'deposit',
+                metadata: {
+                  external_reference: nextAction.data.reference,
+                },
+                trx,
+              });
+
+              if (!creditResult.success) {
+                await trx.rollback();
+
+                return {
+                  success: false,
+                  error: creditResult.error,
+                };
+              }
+
+              await trx.commit();
+
+              res.status(200).json({
+                status: 'success',
+                data:{
+                  message: 'your account has been funded'
+                }
+              })
+            }
+
+          } catch (error) {
+            
+            await trx.rollback();
+
+            res.status(500).json({
+              status: 'error',
+              error: 'something unexpected happened'
+            
+            });
+          }
+      
+      } catch (error) {
+
+          console.error(error, 'error')
+
+          if (error.response){
+            console.error(error.response.data, 'error axios')
+            return res.status(404).res.json({
+              status : 'error',
+              error: error.response.data.message
+            })
+          }
+          
           res.status(500).json({
-            status: 'error',
-            error: 'something unexpected happened'
-          
-          });
-        }
-    
-    } catch (error) {
-
-        console.error(error, 'error')
-
-        if (error.response){
-          console.error(error.response.data, 'error axios')
-          return res.status(404).res.json({
-            status : 'error',
-            error: error.response.data.message
+              status: 'error',
+              error: 'something went wrong'
           })
-        }
-        
-        res.status(500).json({
-            status: 'error',
-            error: 'something went wrong'
-        })
+      }
+    }else{
+      res.status(404).json({
+        status: 'error',
+        error: 'account does not exist'
+      });
     }
 }
