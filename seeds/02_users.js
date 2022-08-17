492adde53e1905ef05c84d4cdde1791a65c38209
FuFuWarrior <akpuwarrior@gmail.com>
2022-08-17 09:16:46 +0100

    made changes to seed file

--- seeds/02_users.js
+++ seeds/02_users.js
@@ -2,11 +2,6 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> } 
  */
- const firstName = faker.name.firstName();
- const lastName = faker.name.lastName();
- const email = faker.internet.email( firstName, lastName );
- const phoneNumber = faker.random.numeric(11);
- const bvn = faker.random.numeric(11);
 
  exports.seed = async function(knex) {
     // Deletes ALL existing entries
@@ -14,13 +9,13 @@
     await knex('users').insert([
         {
             id: 1,
-            first_name: faker.name.firstName(),
-            last_name: faker.name.lastName(),
-            email: 'email@email.com',
+            first_name: 'Tinubu',
+            last_name: 'Obi',
+            email: 'tinubu@obi.com',
             dob: '1/2/2002',
             address: 'no 2 okolathom lalou',
-            phone_number: faker.random.numeric(11),
-            bvn: faker.random.numeric(11)
+            phone_number: '4839483',
+            bvn: '389284547'
         },
     ]);
   };
